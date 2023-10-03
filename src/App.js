83d3986d0cce0f3d0414303f74ec7f1ee717305a
sanjiinvo <sanzhar_sattybaev@mail.ru>
2023-10-03 20:38:03 +0600

    first com

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.scss';
+import zhennev from './img/zhennev.png'
 
 function App() {
+  const [clicked, setcliecked] = useState(false)
+  const [mainopen, setmainopen] = useState(false)
+const HandleClick = () =>{
+  setcliecked(!clicked)
+  console.log(clicked);
+}
+const OpenMain = () => {
+setmainopen(!mainopen)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={clicked? 'main-block main-block-opened':'main-block '}>
+      <div className='first-title-main'>
+        <p className='A-name'>Aмирхан</p>
+        <p className='nd-name'>&</p>
+        <p className='K-name'>Kамила</p>
+      </div>
+
+      </div>
+      <button className='button' onClick={HandleClick}>
+        click
+      </button>
+<div class="section">
+  <div class="envelope">
+    
+  <div onClick={HandleClick} class={!clicked ? "envelope__top envelope__top_close" : 'envelope__top'}>
+    <img src="http://up-skills.ru/test/envelope-top.svg" alt=""/>
+    </div>   
+    
+    <div  class={!clicked ? "paper paper_close" : 'paper'}>
+      <div className='first-title'>
+        <p className='A-name'>Aмирхан</p>
+        <p className='nd-name'>&</p>
+        <p className='K-name'>Kамила</p>
+      </div>
+      <img className='first-title-img' src={zhennev} alt='zhenev'/>
+
+    </div>
+    
+    <div onClick={OpenMain} class="envelope__body">
+      <img src="http://up-skills.ru/test/envelope.svg" alt=""/>
+    </div>
+    
+  </div>
+</div>
     </div>
   );
 }
